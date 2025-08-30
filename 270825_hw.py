f2f96ddb3d0846b54953c865614da975d29c8e9a
Volodymyr Potereiko(AIPython52) <potereyko@gmail.com>
2025-08-30 12:34:17 +0300

    Add initial project setup: .gitignore, README  and 270825_hw.py update

---
+++ 270825_hw.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Курс: AI+Python
+# Модуль 11. ООП
+# Тема: ООП. Частина 3
+#  Завдання 1
+# Створіть наступні класи:
+#  CreditCardPayment – атрибути currency
+#  PayPalPayment – атрибути currency
+#  CryptoPayment – атрибути currency
+# Методи:
+#  pay(amount) – виводить повідомлення
+# o CreditCardPayment – оплата карткою {amount}{currency}
+# o PayPalPayment – оплата PayPal {amount}{currency}
+# o CryptoPayment – оплата криптогаманцем {amount}{currency}
+# Напишіть функцію create_payment() яка запитує у
+# користувача тип рахунку та потрібні атрибути і повертає
+# об’єкт.
+# Створіть декілька рахунків, добавте їх у список та для
+# кожної викличте відповідні методи.
